fd195c9840b38c8e3a02f195d37ad6b1aad3918d
cxb0dly0zz <cxb0dly0zz@163.com>
2015-06-24 16:05:27 +0800

    Initial commit

---
+++ app/src/main/java/com/tu/tu/TopFragment.java
@@ -0,0 +1,17 @@
+package com.tu.tu;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TopFragment extends Fragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View contactsLayout = inflater.inflate(R.layout.top_layout,
+				container, false);
+		return contactsLayout;
+	}
+}
